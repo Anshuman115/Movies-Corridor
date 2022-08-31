ac2795a429217a0fe23a57ff42315e534454f997
Anshuman115 <anshumantripathy123@gmail.com>
2022-09-01 00:07:08 +0530

    made the initial build

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, Profile, Navbar } from '.';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
-      App
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+// Steps for developing a react project
+// 1. setup the project,
+// 2. then add all the dependencies
+// 3. setup all the pages using route and routes .
+// 4. have browser router in index.js
+// 5. Switch is replaced by routes and route
+// requires an element parameter, and no need of exact.
+// 6. get started with the components , make all
+// the components folder and their jsx
+// add all the components in their routes
+// 7. make an .env file to stop dev error due to eslint
