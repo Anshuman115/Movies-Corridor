b4b64b6d05abdf8ddbbd8f52968c9639e8f01ed0
Anshuman115 <anshumantripathy123@gmail.com>
2022-09-03 01:46:22 +0530

    Added redux stuffs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+
+// import createRoot from 'react-dom/client';
 
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
 );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <ThemeProvider theme={theme}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </ThemeProvider>
+//   </Provider>,
+//   document.getElementById('root'),
+// );
